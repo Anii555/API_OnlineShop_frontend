90096dd34c26fd78d74c4710464a5b9ee222bc75
Anii555 <13anna96@gmail.com>
2022-04-06 18:17:55 +0400

    feature/DEMO-26_28

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,8 @@
 <template>
   <!-- аля html -->
-  <!-- <div
-    :style="{ backgroundImage: 'url(' + require('@/assets/meat.jpg') + ')' }"
-  > -->
   <div style="width: 70%; max-width: 1200px; margin: 30px auto">
     <div id="app">
       <h1 class="display-2 font-weight-bold mb-3">Продуктовая лавка</h1>
-
-      <!-- <v-img
-        src="https://sun9-44.userapi.com/impg/hLDGW6FL2wCqoRhKSYMeBUmJz2Rww8nsn3oYDg/v0p7e46upww.jpg?size=1080x1080&quality=95&sign=8a2db2e12c7bc12a2664f5217500bf6d&type=album"
-        aspect-ratio="2"
-      ></v-img>
-      <div class="subheading pt-4"></div> -->
       <div>
         <h1>Корзинка</h1>
         <v-simple-table dense>
@@ -24,9 +15,9 @@
             </thead>
             <tbody>
               <tr>
-                <!-- а могу просто забиндить v-bind="Basket" v-for="item in Basket" :key="item.name"-->
-                <td>{{ Basket.name }}</td>
-                <td>{{ Basket.price }}</td>
+                <!-- а могу просто забиндить v-bind="basket" v-for="item in basket" :key="item.name"-->
+                <td>{{ basket.name }}</td>
+                <td>{{ basket.price }}</td>
               </tr>
             </tbody>
           </template>
@@ -34,7 +25,7 @@
       </div>
 
       <div class="text-center">
-        <v-btn @click="AddInBasket" class="mx-2" fab dark large color="purple">
+        <v-btn @click="addInBasket" class="mx-2" fab dark large color="purple">
           <v-icon dark> mdi-android </v-icon>
           BUY
         </v-btn>
@@ -110,27 +101,23 @@ export default {
           unitPrice: "Цена",
         },
       ],
-      Basket: [],
+      basket: [],
       sel: null,
     };
   },
 
   methods: {
-    AddInBasket() {
-      // const NewProductOfBasket = {
-      this.Basket = {
+    addInBasket() {
+      // const newProductOfBasket = {
+      this.basket = {
         name: this.sel.productName,
         price: this.sel.unitPrice,
       };
-      //this.Basket.push(NewProductOfBasket);
+      //this.basket.push(newProductOfBasket);
 
       alert(
-        `currentProduct, ${this.Basket}! товар: ${this.Basket.name} цена: ${this.Basket.price}`
+        `currentProduct, ${this.basket}! товар: ${this.basket.name} цена: ${this.basket.price}`
       );
-      //console.log(currentProduct);
-
-      //localStorage.setItem("cryptonomicon-list", JSON.stringify(this.tickers));
-      //this.subscribeToUpdates(currentProduct.name);
     },
 
     created() {
@@ -139,26 +126,21 @@ export default {
         (this.response.unitPrice = "555"),
         axios
           .post("http://localhost:5090/product", this.response)
-          //.then((response) => (this.productName = response.data.productName));
           .then((rez) => {
             console.log(rez);
             this.response = JSON.stringify(rez.data); //для массива
-            //this.response = rez.data;
-            //this.productId = this.resp.data;
           });
       alert(this.response);
     },
 
-    add: function () {
+    add() {
       axios
         .post("http://localhost:5090/product", {
           body: this.response,
         })
         .then((resp) => {
           console.log(resp);
-          //this.response = JSON.stringify(resp.data); //для массива
           this.response = resp.data;
-          //this.productId = this.resp.data;
         })
         .catch((e) => {
           console.error(e);
@@ -166,7 +148,7 @@ export default {
     },
 
     select(currentProduct) {
-      this.Basket = currentProduct;
+      this.basket = currentProduct;
       this.sel = currentProduct;
       alert(this.sel.productName); //"Вывод выбранного товара"
     },
@@ -178,9 +160,7 @@ export default {
       .get("http://localhost:5090/product")
       .then((resp) => {
         console.log(resp);
-        //this.response = JSON.stringify(resp.data); //для массива
         this.response = resp.data;
-        //this.productId = this.resp.data;
       })
       .catch((e) => {
         console.error(e);
