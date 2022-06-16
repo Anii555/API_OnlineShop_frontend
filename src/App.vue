b41ab2b148b30c9065817a8ff52450f8d1e3da6b
Anii <13anna96@gmail.com>
2022-06-16 12:15:08 +0300

    DEMO-36 тру

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,12 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="item in response" :key="item.name" v-bind="item">
+            <tr
+              v-for="item in response"
+              :key="item.name"
+              v-bind="item"
+              @click="selectProduct(item)"
+            >
               <td>{{ item.productName }}</td>
               <td>{{ item.unitPrice }}</td>
               <td>
@@ -36,6 +41,112 @@
         </template>
       </v-simple-table>
       <!-- Таблица с продуктами -->
+      <template>
+        <v-container fluid>
+          <v-data-iterator
+            :items="response"
+            :items-per-page.sync="infoCardPerPage"
+            hide-default-footer
+          >
+            <template v-slot:header>
+              <v-toolbar class="mb-2" color="indigo darken-5" dark flat>
+                <v-toolbar-title>Продуктовая лавка 2.0</v-toolbar-title>
+              </v-toolbar>
+            </template>
+
+            <template>
+              <v-row>
+                <v-col cols="12" sm="8" md="2" lg="7">
+                  <v-card>
+                    <!-- Начало удачной вставки-->
+                    <v-simple-table height="300px">
+                      <template v-slot:default>
+                        <thead>
+                          <tr>
+                            <th class="text-left">Продукт</th>
+                            <th class="text-left">Цена</th>
+                            <th class="text-left">Мульон кнопок</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr
+                            v-for="item in response"
+                            :key="item.name"
+                            v-bind="item"
+                            @click="selectProduct(item)"
+                          >
+                            <td>{{ item.productName }}</td>
+                            <td>{{ item.unitPrice }}</td>
+                            <td>
+                              <v-btn
+                                @click="addInBasket(item.productId)"
+                                class="mx-2 mb-2"
+                                fab
+                                dark
+                                large
+                                color="purple"
+                              >
+                                <v-icon dark> mdi-android </v-icon>
+                                BUY
+                              </v-btn>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </template>
+                    </v-simple-table>
+                  </v-card>
+                </v-col>
+                <!-- Конец удачной вставки-->
+                <v-col cols="12" sm="4" md="2" lg="5">
+                  <v-card v-if="sel" class="relative">
+                    <v-card-title class="subheading font-weight-bold">
+                      Информация о продукте
+                    </v-card-title>
+
+                    <v-divider></v-divider>
+
+                    <v-list dense>
+                      <v-list-item>
+                        <v-list-item-content>ID:</v-list-item-content>
+                        <v-list-item-content class="align-end">
+                          {{ sel.productId }}
+                        </v-list-item-content>
+                      </v-list-item>
+
+                      <v-list-item>
+                        <v-list-item-content>Название:</v-list-item-content>
+                        <v-list-item-content class="align-end">
+                          {{ sel.productName }}
+                        </v-list-item-content>
+                      </v-list-item>
+
+                      <v-list-item>
+                        <v-list-item-content>Цена:</v-list-item-content>
+                        <v-list-item-content class="align-end">
+                          {{ sel.unitPrice }}
+                        </v-list-item-content>
+                      </v-list-item>
+
+                      <v-list-item>
+                        <v-list-item-content>Кол-во:</v-list-item-content>
+                        <v-list-item-content class="align-end">
+                          {{ sel.quantityPerUnit }}
+                        </v-list-item-content>
+                      </v-list-item>
+                    </v-list>
+                  </v-card>
+                </v-col>
+              </v-row>
+            </template>
+
+            <template v-slot:footer>
+              <v-toolbar class="mt-2" color="indigo" dark flat>
+                <v-toolbar-title class="subheading"> </v-toolbar-title>
+              </v-toolbar>
+            </template>
+          </v-data-iterator>
+        </v-container>
+      </template>
     </div>
   </div>
 </template>
@@ -53,16 +164,22 @@ export default {
           productId: "ID Продукта",
           productName: "Название",
           unitPrice: "Цена",
+          quantityPerUnit: "Кол-во за единицу",
         },
       ],
       basket: [],
-      sel: undefined,
+      sel: {},
+      infoCardPerPage: 1,
     };
   },
 
   methods: {
     addInBasket(id) {
-      axios.post(`http://localhost:5090/product/${id}`);
+      axios.post(`http://localhost:5090/cart/${id}`);
+    },
+
+    selectProduct(selectItem) {
+      this.sel = selectItem;
     },
   },
 
